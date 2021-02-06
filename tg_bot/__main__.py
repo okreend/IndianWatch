c8cbb5ff27bc5a4134fc39ffc36cd70775a14cec
okreend <77939839+okreend@users.noreply.github.com>
2021-02-06 22:50:03 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -126,17 +126,6 @@ def start(bot: Bot, update: Update, args: List[str]):
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
-                        else:
-            first_name = update.effective_user.first_name
-            update.effective_message.reply_photo(
-                SAITAMA_IMG,
-                PM_START_TEXT.format(
-                    escape_markdown(first_name),
-                    escape_markdown(context.bot.first_name)),
-                parse_mode=ParseMode.MARKDOWN,
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup(
-
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
