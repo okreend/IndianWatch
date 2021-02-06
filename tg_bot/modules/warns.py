4c3eaede445934551c12050d0b435a1def26b58c
okreend <77939839+okreend@users.noreply.github.com>
2021-02-07 01:21:23 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -62,9 +62,9 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
                                                                   mention_html(user.id, user.first_name), 
                                                                   reason, num_warns, limit)
 
-    else:
+    else:http://t.me/IndianWatchRobot?start=-1001336053099
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Remove warn", callback_data="rm_warn({})".format(user.id))]])
+            [[InlineKeyboardButton("Rules", callback_data="http://t.me/IndianWatchRobot?start=-1001336053099".format(user.id))]])
 
         reply = "{} has {}/{} warnings... watch out!".format(mention_html(user.id, user.first_name), num_warns,
                                                              limit)
